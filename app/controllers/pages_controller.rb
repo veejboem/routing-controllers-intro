2014c388425d29fe9833a26b1b21071b8225e041
Valerie Boem <vboem@gmail.com>
2017-01-30 15:12:33 -0500

    Set params for secrets page & redirected to kittens page if requirements are not met.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,4 +25,11 @@ class PagesController < ApplicationController
     requested_size = params[:size]
     @kitten_url = @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
+
+  def secrets
+    if params[:magic_word] != "banana"
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to '/kittens/:size/:size'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ get '/kitten/:size/:size' => 'pages#kitten'
 
 get '/kittens/:size/:size' => 'pages#kittens'
 
+get '/secrets/:magic_word' => 'pages#secrets'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
